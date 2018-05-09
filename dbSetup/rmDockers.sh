de484d9426966c6b0e0d5974e3ca297f181ffd37
Allanwjm <allan_wu_1@126.com>
2018-05-10 04:59:02 +1000

    Update rmDockers.sh

--- dbSetup/rmDockers.sh
+++ dbSetup/rmDockers.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# COMP90024 Team 4 Group Assignment, Melbourne
+# Hongxiang Yang: 674136 hongxiangy@student.unimelb.edu.au
+# Jiaming Wu: 815465 jiamingw@student.unimelb.edu.au
+# Qingyang Li: 899636 qingyangl4@student.unimelb.edu.au
+# Xueyao Chen: 851312 xueyaoc@student.unimelb.edu.au
 
 sudo docker stop $(sudo docker ps -a -q)
 #sudo docker rm $(sudo docker ps -a -f status=exited -q)
