28d7dbe3ac9ba25d6e95dc0661f39337ac43d380
phillischen <32267592+phillischen@users.noreply.github.com>
2018-05-10 05:00:58 +1000

    Update installDocker.sh

--- serverSetup/remote_backup/installDocker.sh
+++ serverSetup/remote_backup/installDocker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# COMP90024 Team 4 Group Assignment, Melbourne
+# Hongxiang Yang: 674136 hongxiangy@student.unimelb.edu.au
+# Jiaming Wu: 815465 jiamingw@student.unimelb.edu.au
+# Qingyang Li: 899636 qingyangl4@student.unimelb.edu.au
+# Xueyao Chen: 851312 xueyaoc@student.unimelb.edu.au
 apt-get update
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
