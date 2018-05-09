d7cb9c79fb4ff25c019d353d3c696362de101b68
Allanwjm <allan_wu_1@126.com>
2018-05-10 05:01:22 +1000

    Update stopserver.sh

--- app/web/stopserver.sh
+++ app/web/stopserver.sh
@@ -1,1 +1,6 @@
+# COMP90024 Team 4 Group Assignment, Melbourne
+# Hongxiang Yang: 674136 hongxiangy@student.unimelb.edu.au
+# Jiaming Wu: 815465 jiamingw@student.unimelb.edu.au
+# Qingyang Li: 899636 qingyangl4@student.unimelb.edu.au
+# Xueyao Chen: 851312 xueyaoc@student.unimelb.edu.au
 screen -S webserver -X kill
