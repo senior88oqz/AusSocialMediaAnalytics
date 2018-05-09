b049b79fe4c36fea5517d0091ba7c180982eeadb
phillischen <32267592+phillischen@users.noreply.github.com>
2018-05-10 04:59:46 +1000

    Update launch_control.py

--- serverSetup/controlMachine/launch_control.py
+++ serverSetup/controlMachine/launch_control.py
@@ -1,6 +1,11 @@
 import time
 from control_login import ec2_conn
 
+# COMP90024 Team 4 Group Assignment, Melbourne
+# Hongxiang Yang: 674136 hongxiangy@student.unimelb.edu.au
+# Jiaming Wu: 815465 jiamingw@student.unimelb.edu.au
+# Qingyang Li: 899636 qingyangl4@student.unimelb.edu.au
+# Xueyao Chen: 851312 xueyaoc@student.unimelb.edu.au
 # security group setting: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/security-group-rules-reference.html
 
 ec2_conn.run_instances('ami-00003837',
